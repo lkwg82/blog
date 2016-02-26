2de03efd26deeef35d70237c1afd13c7f9133178
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-02-26 23:06:14 +0100

    cleanup, removed old site directory

--- site/docker-run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CID=$(docker build . | tail -n1 | cut -d\  -f3)
-docker run -v `pwd`:/jekyll -p "4000:4000" -ti $CID \
-   bundler install --path .bundler \
-   && bundler exec guard

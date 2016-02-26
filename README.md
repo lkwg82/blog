012b821ac043b58db93793f17c17e93a692b21f9
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-02-26 23:10:07 +0100

    cleared master branch and added hint to gh-pages branch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 this is the repository to track the blog itself and related code examples
 
 see http://blog.lgohlke.de
+
+see branch gh-pages
--- site/docker-run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-image="blog-jekyll"
-docker build -t ${image} .
-docker run -v `pwd`:/jekyll -p "4000:4000" -ti ${image}

d06584b46176abf1b381d326b938bef660e011e8
Lars K.W. Gohlke <1891171+lkwg82@users.noreply.github.com>
2025-12-23 09:25:44 +0100

    adjust to podman

--- docker-run.sh
+++ docker-run.sh
@@ -4,6 +4,10 @@ set -e
 
 image="blog-jekyll"
 docker build -t ${image} .
-docker run -v "$PWD":/jekyll -p "127.0.0.1:4000:4000" \
+
+# https://www.redhat.com/en/blog/rootless-podman-user-namespace-modes
+# podman needs userns
+
+docker run -v "$PWD":/jekyll --userns=keep-id -p "127.0.0.1:4000:4000" \
   -p "127.0.0.1:35729:35729" \
   -ti ${image} $@
